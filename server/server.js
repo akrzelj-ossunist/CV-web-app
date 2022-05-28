67f4180e7034923323a7dac4488400f88e1392a6
akrzelj-ossunist <92447214+akrzelj-ossunist@users.noreply.github.com>
2022-05-28 16:46:22 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-const { v4: uuidv4 } = require('uuid');//Generates random id
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const userTable = require('./models/userData')//Import table we created in Data.js
+const userTable = require('./models/Data')//Import table we created in Data.js
 const mongoose = require('mongoose')
 const url = 'mongodb+srv://Antonio:Antonio@cluster0.j67lfzf.mongodb.net/User?retryWrites=true&w=majority';
 
@@ -19,21 +18,26 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+let allowPost = 1;//Stops double posting so we dont make dupes in database
 app.post("/saveUser", function (req, res) {
     //This is how you save data into table in mongoDB
-    const addUser = new userTable({
-        id: uuidv4(),
-        firstName: req.body.fname,
-        lastName: req.body.lname,
-        phone: req.body.phone,
-        email: req.body.email,
-        address: req.body.address,
-        gender: req.body.gender,
-        jobs: req.body.jobs,
-        education: req.body.education
-    })
-    addUser.save()
-    .catch(error => console.log(error))
+    if(allowPost){
+        console.log('Adding')
+        const addUser = new userTable({
+            firstName: req.body.fname,
+            lastName: req.body.lname,
+            phone: req.body.phone,
+            email: req.body.email,
+            address: req.body.address,
+            gender: req.body.gender,
+            jobs: req.body.jobs,
+            education: req.body.education
+        })
+        addUser.save()
+        allowPost = 0;
+    }
+    else
+        allowPost = 1;
 })
 
 // We can query like this in this case we were looking for guy named Antonio Zvonko
