63af98b76d6f18fc51860d4b0d14d4f2bc270d8f
akrzelj-ossunist <92447214+akrzelj-ossunist@users.noreply.github.com>
2022-05-28 02:46:49 +0200

    mongo connection

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const fs = require('fs');
+const userTable = require('./models/Data')//Import table we created in Data.js
+const mongoose = require('mongoose')
 
-//use cors to allow cross origin resource sharing otherwise its blocked
+//You can get this link from mongoDB website and this is how to connect to mongo site
+mongoose.connect('mongodb+srv://Antonio:Antonio@cluster0.j67lfzf.mongodb.net/User?retryWrites=true&w=majority', () => console.log('Connected'))
+
 app.use(
     cors({
       origin: 'http://localhost:3000',
@@ -14,8 +18,18 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//With post we get data from frontend
 app.post("/saveUser", function (req, res) {
+    //This is how you save data into table in mongoDB
+    const addUser = new userTable({
+        firstName: req.body.fname,
+        lastName: req.body.lname,
+        phone: req.body.phone,
+        email: req.body.email,
+        address: req.body.address,
+        gender: req.body.gender,
+    })
+    addUser.save()
+    //This is how u save data in json file
     let newUser = {
         fname: req.body.fname,
         lname: req.body.lname,
